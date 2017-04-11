4ed38c9bff983de007cf65b03d74dd00eb626d43
Sanjay Shah <sanjay_y2k@yahoo.com>
2017-04-11 03:23:33 -0700

    added z-nopm-util

---
+++ z-nomp-util/util/remove_claymore_fees.sh
@@ -0,0 +1,1 @@
+/usr/local/bin/redis-cli hdel zclassic:balances t1b9PsiekL4RbMoGzyLMFkMevbz7QfwepgP t1N7NByjcXxJEDPeb1KBDT9Q8Wocb3urxnv t1dn3KXy6mBi5TR1ifRwYse6JMgR2w7zUbr t1W9HL5Aep6WHsSqHiP9YrjTH2ZpfKR1d3t > ~/z-nomp/util/remove_claymore_fees_last_result.txt
